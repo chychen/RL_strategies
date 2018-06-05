452cb76d139dfc0ebd1080fbf42acf782c6a1564
Jay Chen <jaycase.cs01@gmail.com>
2018-06-05 22:15:15 +0800

    reset ppo optimizer

--- bball_strategies/scripts/gail/train.py
+++ bball_strategies/scripts/gail/train.py
@@ -402,6 +402,10 @@ def train(config, env_processes, outdir):
             sess, saver, config.logdir, resume=FLAGS.resume)
         # NOTE reset variables in optimizer
         D.reset_optimizer(sess)
+        # reset PPO optimizer
+        opt_reset = tf.group(
+            [v.initializer for v in graph.algo._optimizer.variables()])
+        sess.run(opt_reset)
         # visulization stuff
         if FLAGS.tally_only:
             tally_reward_line_chart(config, sess.run(D._global_steps), ppo_policy, D, denormalize_observ, normalize_observ)
